b107d1730af51c4690fde2e99e37ea8882731c01
yahui.wei <yahui.wei@hand-china.com>
2020-08-25 14:32:53 +0800

    README.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,23 @@
 module.exports = {
     title: 'Hello VuePress',
     description: 'blog.doacme.com',
-    base: '/'
+    base: '/',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/server/' },
+            { text: 'blog', link: 'http://blog.doacme.com' },
+        ],
+        sidebar: [
+            {
+                title: 'Group 1',   // 必要的
+                path: '/server/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                  '/linux.html', '/nginx'
+                ]
+              },
+        ],
+    },
 }
